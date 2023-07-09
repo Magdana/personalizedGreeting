c1b1e6e3301e75bb980a6929d839ba700945afc7
Magda <magdagvirjishvili@gmail.com>
2023-07-10 00:46:37 +0400

    magdas_commit

---
+++ personalizedGreeting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your name  ");
+        string greeting = ("Hello " + Console.ReadLine()+"!, welcome to my program.");
+        Console.WriteLine(greeting);
+    }
+}
+
